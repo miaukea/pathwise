97e1ef3cd8df7865cc5ab4dd0a3b157bc5a6e7ca
howellea <howelleddy@gmail.com>
2025-02-04 19:38:35 -0600

    completed trips table

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,5 +1,5 @@
 import sequelize from '../config/connection.js';
-import { VolunteerFactory } from './volunteer.js';
+import { VolunteerFactory } from './Trips.js';
 
 const Volunteer = VolunteerFactory(sequelize);
 console.log(Volunteer === sequelize.models.Volunteer);
