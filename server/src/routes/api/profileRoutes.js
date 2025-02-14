fce5bdffe4d6e26ddc1f90841633e0555dd3d5bb
Melody <meliaukea@gmail.com>
2025-02-13 19:22:09 -0500

    Fixed minor mistakes in files

--- server/src/routes/api/profileRoutes.js
+++ server/src/routes/api/profileRoutes.js
@@ -1,20 +1,20 @@
-import { Router, req, res } from 'express';
+import { Router } from 'express';
 import bcrypt from 'bcrypt';
 import { Profile } from '../../models/model_index.js';
 
 const router = Router();
 
-app.get('/', (req, res) => {
+router.get('/', (res) => {
   res.send('');
 });
 
 // CREATE a new profile
-app.create('/', async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const newProfile = req.body;
     newProfile.password = await bcrypt.hash(req.body.password, 10);
     const userData = await Profile.create(newProfile);
-    res.status(200).json(userData);
+    res.status(200).xjson(userData);
   } catch (err) {
     res.status(400).json(err);
   }
