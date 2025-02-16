a5148c5bd3106cd9074a097aeeba78bcb912ad4c
mckenzie balaka <zakabalaka@yahoo.com>
2025-02-16 14:08:49 -0500

    frontpage

--- client/src/api/requestHandler.js
+++ client/src/api/requestHandler.js
@@ -1,14 +1,13 @@
 export const requestHandler = async (url, method, options, data) => {
-    const response = await fetch(url, {
-      method: method,
-      ...options,
-      body: JSON.stringify(data),
-    });
+  const response = await fetch(url, {
+    method: method,
+    ...options,
+    body: data ? JSON.stringify(data) : undefined, // Only stringify if data is defined
+  });
 
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
 
-    return await response.json();
-  };
-
+  return await response.json();
+};
