a4ca4ec0f3aa44e10cd9d83d337ec977f9aff994
howellea <howelleddy@gmail.com>
2025-02-18 01:06:45 -0600

    scafold for front end added & test data

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,14 +1,18 @@
 import axios from 'axios';
 
-const getUsers = () => {
+const getTrips = () => {
+  return axios.get('https://my-json-server.typicode.com/howellea/pathwisedata/trips');
+};
+
+const getDestinaton = () => {
   return axios.get('https://jsonplaceholder.typicode.com/users/');
 };
 
-const getSingleUser = (paramId) => {
-  return axios.get(`https://jsonplaceholder.typicode.com/users/${paramId}`);
+const getSingleDestination = (paramId) => {
+  return axios.get(`https://my-json-server.typicode.com/howellea/pathwisedata/destinations${paramId}`);
 };
 
 export default {
-  getUsers,
-  getSingleUser,
+  getTrips,
+  getSingleDestination,
 };
